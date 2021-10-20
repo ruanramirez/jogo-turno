009bf4ef05b8a1e0943b5a416156fd43b8515a93
ruanramirez <ruanramirez01@gmail.com>
2021-10-20 17:58:54 -0300

    Added heal

--- app.js
+++ app.js
@@ -23,7 +23,15 @@ new Vue({
 		hurt(prop, min, max, special) {
 			const plus = special ? 5 : 0
 			const hurt = this.getRandom(min + plus, max + plus)
-			this[prop] = Math.max(this.playerLife - hurt, 0)
+			this[prop] = Math.max(this[prop] - hurt, 0)
+		},
+		healAndHurt() {
+			this.heal(10, 15)
+			this.hurt('playerLife', 7, 12, false)
+		},
+		heal(min, max) {
+			const heal = this.getRandom(min, max)
+			this.playerLife = Math.min(this.playerLife + heal, 100)
 		},
 		getRandom(min, max) {
 			const value = Math.random() * (max - min) + min
